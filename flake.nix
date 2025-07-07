2681cb144d7963f989e90082fb4b0db54044db48
Guillaume Fournier <me@guif.dev>
2025-07-07 20:56:00 +0200

    Add flake to build zola

--- flake.nix
+++ flake.nix
@@ -16,6 +16,21 @@
         pkgs = (import nixpkgs) { inherit system; };
       in
       {
+        packages = {
+          default = pkgs.stdenv.mkDerivation {
+            name = "guif-landingpage";
+            src = ./.;
+            buildInputs = [ pkgs.zola ];
+            buildPhase = ''
+              zola build --force
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp -r public $out/website
+            '';
+          };
+        };
+
         devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             zola
